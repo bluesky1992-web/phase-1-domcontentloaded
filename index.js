9b01e84dd9e2a9741afe9d2f0185226255bba1c0
Ali ibrahim jasim <ali.ibrahim.jasim@gmail.com>
2022-10-19 13:22:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+     const ptext = document.getElementById("text")
+     ptext.innerHTML = "This is really cool!"
+     
+})
